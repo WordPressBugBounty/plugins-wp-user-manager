f31593297d87dca6483a5114b15911b0d78cd895
Sean Murphy <sean@iamseanmurphy.com>
2026-04-11 17:20:31 +0000

    2.9.16

--- wp-user-manager/includes/fields/class-wpum-field.php
+++ wp-user-manager/includes/fields/class-wpum-field.php
@@ -209,20 +209,18 @@ class WPUM_Field {
 		}
 
 		foreach ( $field as $key => $value ) {
-			switch ( $key ) {
-				default:
-					$this->$key = $value;
-					break;
+			if ( property_exists( $this, $key ) ) {
+				$this->$key = $value;
 			}
 		}
 
 		if ( ! empty( $this->id ) ) {
 
-			$this->is_primary    = $this->set_as_primary_field( $this->type );
-			$this->required      = $this->get_meta( 'required' );
-			$this->visibility    = $this->get_meta( 'visibility' );
-			$this->editable      = $this->get_meta( 'editing' );
-			$this->parent_id     = max( 0, (int) $this->get_meta( 'parent_id' ) );
+			$this->is_primary = $this->set_as_primary_field( $this->type );
+			$this->required   = $this->get_meta( 'required' );
+			$this->visibility = $this->get_meta( 'visibility' );
+			$this->editable   = $this->get_meta( 'editing' );
+			$this->parent_id  = max( 0, (int) $this->get_meta( 'parent_id' ) );
 
 			$class = 'WPUM_Field_' . ucfirst( $this->get_type() );
 			if ( class_exists( $class ) ) {
